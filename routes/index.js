e6380786b2e1af559111ebab4faf72a29cf4e01b
Thean Hoogenboezem <theunis.hoogenboezem@standardbank.co.za>
2018-06-30 23:33:38 +0200

    Finished implementing the retrievel and setting of currencies in the form  * Next step working towards performing the conversion

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, resApp, next) {
@@ -8,38 +7,47 @@ router.get('/', function(req, resApp, next) {
     let baseUrl = 'https://free.currencyconverterapi.com/api/v5/';
 
     function getCurrencies() {
+        let https = require('https');
         let url = baseUrl + 'currencies';
+        return new Promise( (resolve, reject) => {
+            https.get(url, function(res) {
+                let body = '';
 
-        https.get(url, function(res) {
-            let body = '';
+                res.on('data', function(chunk) {
+                    body += chunk;
+                    console.log(body);
+                });
 
-            res.on('data', function(chunk) {
-                body += chunk;
-                console.log(body);
+                res.on('end', function() {
+                    try {
+                        //Parse the JSON string
+                        let currenciesResponseJson = JSON.parse(body);
+                        /*console.log('Response: ');
+                        console.log(currenciesResponseJson);
+                        console.log('Results - start');*/
+                        let results = currenciesResponseJson.results;
+                        /*console.log(results);
+                        console.log('Results - end');*/
+                        resolve(Object.keys(results));
+                    } catch (e) {
+                        console.log("Parse error: ", e);
+                        reject("Got an error: ", e.message);
+                    }
+                });
+            }).on('error', function(e) {
+                console.log("Got an error: ", e);
+                reject("Got an error: ", e.message);
             });
-
-            res.on('end', function() {
-                try {
-                    //Parse the JSON string
-                    let currenciesResponseJson = JSON.parse(body);
-                    console.log('Response: ');
-                    console.log(currenciesResponseJson);
-                    console.log('Results - start');
-                    let results = currenciesResponseJson.results;
-                    console.log(results);
-                    console.log('Results - end');
-                    return Object.keys(results);
-                } catch (e) {
-                    console.log("Parse error: ", e);
-                }
-            });
-        }).on('error', function(e) {
-            console.log("Got an error: ", e);
         });
     }
 
-    let currencyKeys = getCurrencies();
-    resApp.render('index', { title: 'Currency Converter App', currencies: currencyKeys });
+    getCurrencies().then( (currencies) => {
+        console.log(currencies);
+        resApp.render('index', { title: 'Currency Converter App', currencies: currencies });
+    }).catch((error) => {
+        resApp.render('index', { title: 'Currency Converter App', error: error, currencies: [] });
+    });
+
 });
 
 
